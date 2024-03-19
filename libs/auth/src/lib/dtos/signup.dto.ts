de7cfaeb141bc1ca7536ab50cacd6018381445e6
Zack3855 <Zack@devcorp.online>
2024-03-19 14:48:06 +0400

    added body descriptions

--- libs/auth/src/lib/dtos/signup.dto.ts
+++ libs/auth/src/lib/dtos/signup.dto.ts
@@ -1,30 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class SignupDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   userName: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   firstName: string;
-
+  @ApiProperty()
   @IsOptional()
   @IsString()
   phone: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   lastName: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   serviceAddress: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   password: string;
--- libs/auth/src/lib/dtos/validate-otp.dto.ts
+++ libs/auth/src/lib/dtos/validate-otp.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class ValidateOtpDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   otp: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   newPassword: string;
